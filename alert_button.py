f3cea587ab1dcd5e23c78a51886473934be78f8d
Raspberry Pi OS <alex12papas@gmail.com>
2026-09-23 11:55:50 -0600

    init commit

---
+++ alert_button.py
@@ -0,0 +1,20 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+print("Alert button monitoring system is now active. Press Ctrl+C to stop.")
+
+button_pressed = False
+try:
+	while True:
+		if GPIO.input(7) == GPIO.HIGH and not button_pressed:
+			print("Someone pressed the alert button!")
+			button_pressed = True
+		elif GPIO.input(7) == GPIO.LOW:
+			button_pressed = False
+			time.sleep(0.1)
+except KeyboardInterrupt:
+	print("\nMonitoring stopped.")
+	GPIO.cleanup()
+
